7bad61eb3e7fddb1dc7cb6281eec0de8a1d2fc9e
Ursu Anatolie <ursu.anato.l.3@gmail.com>
2024-03-14 20:49:48 +0200

    Update head.blade.php

--- resources/views/GameDashboard/Elements/head.blade.php
+++ resources/views/GameDashboard/Elements/head.blade.php
@@ -1,10 +1,41 @@
 <div class="head_class">
-    <a href=""></a>
+    <div style="width: 80%; padding: 0 10px; margin: 0 auto;" class="images-head">
+        <a href=""><img src="{{ asset("game_btns/board.png") }}" alt="Board room"></a>
+        <a href=""><img src="{{ asset("game_btns/team.png") }}" alt="Squad"></a>
+        <a href=""><img src="{{ asset("game_btns/tactics.png") }}" alt="Tactics"></a>
+        <a href=""><img src="{{ asset("game_btns/train.png") }}" alt="Staff"></a>
+        <a href=""><img src="{{ asset("game_btns/matches.png") }}" alt="Staff"></a>
+        <a href=""><img src="{{ asset("game_btns/paper.png") }}" alt="Staff"></a>
+        <a href=""><img src="{{ asset("game_btns/hotlist.png") }}" alt="Hotlist"></a>
+        <a href=""><img src="{{ asset("game_btns/cup_mail_anim.gif") }}" alt="Mail"></a>
+        <a href=""><img src="{{ asset("game_btns/deals.png") }}" alt="Negotiations"></a>
+        <a href=""><img src="{{ asset("game_btns/world.png") }}" alt="Map"></a>
+        <a href=""><img src="{{ asset("game_btns/search.png") }}" alt="Search"></a>
+        <a href=""><img src="{{ asset("game_btns/managers.png") }}" alt="Manager list"></a>
+        <a href=""><img src="{{ asset("game_btns/rankings.png") }}" alt="Rankings"></a>
+        <a href=""><img src="{{ asset("game_btns/forums.png") }}" alt="Managersim Discord"></a>
+        <a href=""><img src="{{ asset("game_btns/videocall.png") }}" alt="talk"></a>
+        <a href=""><img src="{{ asset("game_btns/options.png") }}" alt="Personal information"></a>
+    </div>
 </div>
 
 <style>
     .head_class{
         width: 100%;
-        padding: 15px 0;
+        padding: 30px 0;
+    }
+    .images-head{
+        flex-wrap: wrap;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .images-head img{
+        margin: 0 1px;
+        transition: 0.2s all ease;
+        border-radius: 15px;
+    }
+    .images-head img:hover{
+        opacity: 0.8;
     }
 </style>
